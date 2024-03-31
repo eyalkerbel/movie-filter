fff7fda08ea0ca10709c934df1f8a04a9b181bde
eyalkarbel <107179952+eyalkarbel@users.noreply.github.com>
2024-03-31 14:30:08 +0300

    init

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import VideoCard from './VideoCard';
 import {useVideoContext} from "./context/Video";
+import {Grid} from "@mui/material";
 
 
 const VideoList: React.FC = () => {
   const { filteredVideos } = useVideoContext();
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' , marginTop: '1rem' }}>
-      {filteredVideos.map(video => (
-        <VideoCard key={video.id} video={video} />
+    <Grid container spacing={2} justifyContent="center" style={{marginTop: '1rem'}}>
+      {filteredVideos.map((video) => (
+        <Grid item xs={12} sm={6} md={4} display="flex" justifyContent="center" key={video.id}>
+          <VideoCard video={video} />
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 };
 
